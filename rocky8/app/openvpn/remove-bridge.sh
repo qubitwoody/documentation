2fa69172c40b302a4500bf920a35a0bd5898a93c
Qubit Security Inc <plura@qubitsec.com>
2023-02-10 16:56:16 +0900

    Create remove-bridge.sh

---
+++ rocky8/app/openvpn/remove-bridge.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# network interface which can connect to local network
+IF=enp1s0
+# interface VPN tunnel uses
+# for the case of this example like specifying [tun] on the config, generally this param is [tun0]
+VPNIF=tun0
+# listening port of OpenVPN
+PORT=1194
+
+firewall-cmd --zone=trusted --remove-masquerade
+firewall-cmd --direct --remove-rule ipv4 filter FORWARD 0 -i ${VPNIF} -o ${IF} -j ACCEPT
+firewall-cmd --direct --remove-rule ipv4 nat POSTROUTING 0 -o ${IF} -j MASQUERADE
+firewall-cmd --remove-port=${PORT}/udp
