87e1e4c8cdb89602a3fd915829579c6d6ec2912a
Qubit Security Inc <plura@qubitsec.com>
2023-04-29 14:07:36 +0900

    Update run_network_bridge0.sh

--- rocky8/kvm/run_network_bridge0.sh
+++ rocky8/kvm/run_network_bridge0.sh
@@ -2,8 +2,6 @@ nmcli connection add type bridge autoconnect yes con-name bridge0 ifname bridge0
 
 nmcli connection modify bridge0 ipv4.addresses 10.10.11.10/23 ipv4.method manual
 
-nmcli connection modify bridge0 ipv4.gateway 10.10.10.1
-
 nmcli connection modify bridge0 ipv4.dns 10.10.10.250
 
 nmcli connection del enp2s0f0
