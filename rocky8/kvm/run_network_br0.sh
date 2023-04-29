6d3651a926183192b07d13370245dfecd897efc0
Qubit Security Inc <plura@qubitsec.com>
2023-04-29 13:55:34 +0900

    Update run_network_br0.sh

--- rocky8/kvm/run_network_br0.sh
+++ rocky8/kvm/run_network_br0.sh
@@ -1,10 +1,10 @@
 nmcli connection add type bridge autoconnect yes con-name br0 ifname br0
 
-nmcli connection modify br0 ipv4.addresses 10.10.11.10/23 ipv4.method manual
+nmcli connection modify br0 ipv4.addresses 172.16.16.10/12 ipv4.method manual
 
-nmcli connection modify br0 ipv4.gateway 10.10.10.1
+nmcli connection modify br0 ipv4.gateway 172.16.0.1
 
-nmcli connection modify br0 ipv4.dns 10.10.10.250
+nmcli connection modify br0 ipv4.dns 172.16.10.250
 
 nmcli connection del enp2s0f0
 
